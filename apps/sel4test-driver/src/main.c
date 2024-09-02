c91ac9e48efc7081b5e3cd323eed874621a50293
ZhiyuanSue <2262387848@qq.com>
2024-09-02 16:17:12 +0800

    disable device tree

--- apps/sel4test-driver/src/main.c
+++ apps/sel4test-driver/src/main.c
@@ -423,25 +423,25 @@ void *main_continued(void *arg UNUSED)
 
     /* allocate a piece of device untyped memory for the frame tests,
      * note that spike doesn't have any device untypes so the tests that require device untypes are turned off */
-    if (!config_set(CONFIG_PLAT_SPIKE)) {
-        bool allocated = false;
-        int untyped_count = simple_get_untyped_count(&env.simple);
-        for (int i = 0; i < untyped_count; i++) {
-            bool device = false;
-            uintptr_t ut_paddr = 0;
-            size_t ut_size_bits = 0;
-            seL4_CPtr ut_cptr = simple_get_nth_untyped(&env.simple, i, &ut_size_bits, &ut_paddr, &device);
-            if (device) {
-                error = vka_alloc_frame_at(&env.vka, seL4_PageBits, ut_paddr, &env.device_obj);
-                if (!error) {
-                    allocated = true;
-                    /* we've allocated a single device frame and that's all we need */
-                    break;
-                }
-            }
-        }
-        ZF_LOGF_IF(allocated == false, "Failed to allocate a device frame for the frame tests");
-    }
+    // if (!config_set(CONFIG_PLAT_SPIKE)) {
+    //     bool allocated = false;
+    //     int untyped_count = simple_get_untyped_count(&env.simple);
+    //     for (int i = 0; i < untyped_count; i++) {
+    //         bool device = false;
+    //         uintptr_t ut_paddr = 0;
+    //         size_t ut_size_bits = 0;
+    //         seL4_CPtr ut_cptr = simple_get_nth_untyped(&env.simple, i, &ut_size_bits, &ut_paddr, &device);
+    //         if (device) {
+    //             error = vka_alloc_frame_at(&env.vka, seL4_PageBits, ut_paddr, &env.device_obj);
+    //             if (!error) {
+    //                 allocated = true;
+    //                 /* we've allocated a single device frame and that's all we need */
+    //                 break;
+    //             }
+    //         }
+    //     }
+    //     ZF_LOGF_IF(allocated == false, "Failed to allocate a device frame for the frame tests");
+    // }
 
     /* allocate lots of untyped memory for tests to use */
     env.num_untypeds = populate_untypeds(untypeds);
